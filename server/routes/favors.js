4ac61963e834dcbdfca56e5217ab93435ae306cb
lyndonl3091 <lyndonl3091@gmail.com>
2018-02-08 12:16:33 -0500

    added redux form

--- server/routes/favors.js
+++ server/routes/favors.js
@@ -3,15 +3,13 @@
 const express = require('express');
 const require = require('request');
 
-const User = require('../models/user');
+const User = require('../models/favor');
 
 let router = express.Router();
 
 
-router.get('/', User.authMiddleware, (req, res) => {
-  User.find({}, (err, user) => {
+router.get('/favor', req, res) => {
+  User.find({}, (err, favor) => {
     res.status(err ? 400 : 200).send(err || user);
   });
 });
-
-module.exports = router;
