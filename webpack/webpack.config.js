c1fa52eafc33dd30ea9a64768b97e2274a3945cf
lyndonl3091 <lyndonl3091@gmail.com>
2018-02-15 12:17:08 -0500

    fix webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    path: path.join(__dirname, '/public'),
+    path: path.join(__dirname, '..', '/public'),
     publicPath: '/',
     filename: 'bundle.js'
   },
