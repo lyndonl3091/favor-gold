4472e6470d93f5025d99f6e4da3acf81fa8654fd
lyndonl3091 <lyndonl3091@gmail.com>
2019-08-09 13:10:17 -0400

    add

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,7 @@ export const getData = () => {
     payload: 'test'
   }
 }
+
+export const addFavor = params => {
+  
+}
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,5 @@
 export const GET_FAVORS = 'GET_FAVORS';
+export const ADD_FAVOR = 'ADD_FAVOR'
 
 export const SIGN_UP = 'SIGN_UP';
 export const LOG_IN = 'LOG_IN';
