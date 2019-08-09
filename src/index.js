02b3e9018c25a484fbd6a52973117d3fb4cc1b7b
lyndonl3091 <lyndonl3091@gmail.com>
2019-08-09 19:03:20 -0400

    stable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
